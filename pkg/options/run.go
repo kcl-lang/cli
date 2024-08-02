d51d06d30fe6ee44fa8876d8c13159cf487e2ea6
zongz <zongzhe1024@163.com>
2024-08-02 19:02:15 +0800

    fix: fix missing Print output and bump version 0.9.6
    Signed-off-by: zongz <zongzhe1024@163.com>

--- pkg/options/run.go
+++ pkg/options/run.go
@@ -128,6 +128,7 @@ func (o *RunOptions) Run() error {
 		client.WithDebug(o.Debug),
 		client.WithStrictRange(o.StrictRangeCheck),
 		client.WithCompileOnly(o.CompileOnly),
+		client.WithLogger(o.Writer),
 	)
 
 	if err != nil {
