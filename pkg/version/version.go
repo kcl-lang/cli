1ed44fcfb90b539ba92ab54d6184cdfe8adbdc01
peefy <xpf6677@163.com>
2024-08-02 11:18:00 +0800

    chore: bump version to 0.9.5
    Signed-off-by: peefy <xpf6677@163.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -32,8 +32,9 @@ func getVersion(version string) string {
 }
 
 const (
-	VersionTypeLatest = Version_0_9_4
+	VersionTypeLatest = Version_0_9_5
 
+	Version_0_9_5 VersionType = "0.9.4"
 	Version_0_9_4 VersionType = "0.9.4"
 	Version_0_9_3 VersionType = "0.9.3"
 	Version_0_9_2 VersionType = "0.9.2"
